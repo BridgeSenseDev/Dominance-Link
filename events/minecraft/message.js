1e10438ba3e9485adf66c3851cf8d699c503b78c
BridgeSense <bridgesensemc@gmail.com>
2022-08-05 11:18:58 +0800

    Use webhooks for guild chat

--- events/minecraft/message.js
+++ events/minecraft/message.js
@@ -6,7 +6,8 @@ const {
 const messageToImage = require('../../helper/messageToImage');
 const config = require('../../config.json');
 
-const webhook = new WebhookClient({ url: config.keys.webhookUrl });
+const logWebhook = new WebhookClient({ url: config.keys.logWebhookUrl });
+const gcWebhook = new WebhookClient({ url: config.keys.gcWebhookUrl });
 
 module.exports = {
   async execute(client, message, messagePosition) {
@@ -23,19 +24,23 @@ module.exports = {
       }
     }
     const rawMsg = message.toMotd();
-    await webhook.send({ content: msg, username: 'Matrix Link', avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png' });
+    await logWebhook.send({ content: msg, username: 'Matrix Link', avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png' });
 
     // Guild Chat
     if (msg.indexOf('Online Members:') !== -1) {
       [, onlineMembers] = msg.split('Online Members: ');
     } else if (msg.indexOf('cannot say the same message') !== -1) {
-      await guildChat.send({
+      await gcWebhook.send({
+        username: 'Matrix',
+        avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png',
         files: [messageToImage(
           '§6-------------------------------------------------------------§r §cYou cannot say the same message twice!§6-------------------------------------------------------------',
         )],
       });
     } else if (msg.indexOf('left the guild!') !== -1) {
-      await guildChat.send({
+      await gcWebhook.send({
+        username: 'Matrix',
+        avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png',
         files: [messageToImage(
           `§b-------------------------------------------------------------§r ${rawMsg} §b-------------------------------------------------------------`,
         )],
@@ -43,19 +48,25 @@ module.exports = {
     } else if (msg.indexOf('joined the guild!') !== -1) {
       const name = msg.substring(msg.search(/ (.*?) joined/g) + 1, msg.lastIndexOf(' joined'));
       await bot.chat(`/gc Welcome to Matrix, ${name}! Join our discord using /g discord to learn more about our roles and rules. Our current GEXP requirement is 250k per week.`);
-      await guildChat.send({
+      await gcWebhook.send({
+        username: 'Matrix',
+        avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png',
         files: [messageToImage(
           `§b-------------------------------------------------------------§r ${rawMsg} §b-------------------------------------------------------------`,
         )],
       });
     } else if (msg.indexOf('was promoted') !== -1) {
-      await guildChat.send({
+      await gcWebhook.send({
+        username: 'Matrix',
+        avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png',
         files: [messageToImage(
           `§6-------------------------------------------------------------§r ${rawMsg} §6-------------------------------------------------------------`,
         )],
       });
     } else if (msg.indexOf('Guild >') !== -1) {
-      await guildChat.send({
+      await gcWebhook.send({
+        username: 'Matrix',
+        avatarURL: 'https://cdn.discordapp.com/attachments/986281342457237624/986282015278125157/886245b66dd1d5f5c2469737e58a24ca.png',
         files: [messageToImage(rawMsg)],
       });
       let [, name] = msg.replace(/Guild > |:/g, '').split(' ');
