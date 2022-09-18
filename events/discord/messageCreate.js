0324b78f6409f7e12c403c9657fb0a06f9e5fa96
BridgeSense <bridgesensemc@gmail.com>
2022-09-18 15:24:30 +0800

    Merge verify and link commands

--- events/discord/messageCreate.js
+++ events/discord/messageCreate.js
@@ -4,18 +4,26 @@ const { formatMentions, UUIDtoName } = require('../../helper/utils');
 
 module.exports = {
   execute: async (client, message) => {
+    let uuid;
     if (message.author.bot) return;
     if (message.channel.id === logChannel.id) {
       await bot.chat(message.content);
     } else if (message.channel.id === guildChat.id) {
-      const user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
+      let user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
       if (user === undefined) {
-        const embed = new EmbedBuilder()
-          .setColor(0xe74d3c)
-          .setTitle('Error')
-          .setDescription(`<a:across:986170696512204820> <@${message.author.id}> You must link your discord account to your minecraft account using \`/link\` before talking here`);
-        message.reply({ embeds: [embed] });
-        return;
+        try {
+          ({ uuid } = db.prepare('SELECT uuid FROM members WHERE discord = ?').get(message.author.id));
+        } catch (err) {
+          await message.member.roles.add(message.guild.roles.cache.get('907911526118223912'));
+          const embed = new EmbedBuilder()
+            .setColor(0xe74d3c)
+            .setTitle('Error')
+            .setDescription(`<a:across:986170696512204820> <@${message.author.id}> Please verify first in <#907911357582704640>`);
+          message.reply({ embeds: [embed] });
+          return;
+        }
+        db.prepare('UPDATE guildMembers SET discord = ? WHERE uuid = ?').run(message.author.id, uuid);
+        user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
       }
       message.content = await formatMentions(client, message);
       message.content = message.content.replace(/\n/g, '');
@@ -27,14 +35,20 @@ module.exports = {
       await bot.chat(`/gc ${await UUIDtoName(user.uuid)} ${user.tag}: ${message.content}`);
       await message.delete();
     } else if (message.channel.id === officerChat.id) {
-      const user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
+      let user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
       if (user === undefined) {
-        const embed = new EmbedBuilder()
-          .setColor(0xe74d3c)
-          .setTitle('Error')
-          .setDescription(`<a:across:986170696512204820> <@${message.author.id}> You must link your discord account to your minecraft account using \`/link\` before talking here`);
-        message.reply({ embeds: [embed] });
-        return;
+        const uuid = db.prepare('SELECT uuid FROM members WHERE discord = ?').get(message.author.id);
+        if (uuid === undefined) {
+          await message.author.roles.add(message.guild.roles.cache.get('907911526118223912'));
+          const embed = new EmbedBuilder()
+            .setColor(0xe74d3c)
+            .setTitle('Error')
+            .setDescription(`<a:across:986170696512204820> <@${message.author.id}> Please verify first in <#907911357582704640>`);
+          message.reply({ embeds: [embed] });
+          return;
+        }
+        db.prepare('UPDATE guildMembers SET discord = ? WHERE uuid = ?').run(message.author.id, uuid);
+        user = db.prepare('SELECT uuid, tag FROM guildMembers WHERE discord = ?').get(message.author.id);
       }
       message.content = await formatMentions(client, message);
       message.content = message.content.replace(/\n/g, '');
