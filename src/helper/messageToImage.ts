47d11b868d54c9e6e39dd44426e7bc681052d714
BridgeSense <bridgesensemc@gmail.com>
2022-11-10 22:34:22 +0800

    Add type annotations

--- src/helper/messageToImage.ts
+++ src/helper/messageToImage.ts
@@ -1,31 +1,13 @@
 import { registerFont, createCanvas } from 'canvas';
+import { rgbaColor } from './constants.js';
 
 registerFont('./Uni Sans Heavy Regular.ttf', { family: 'Uni Sans' });
 
-const RGBA_COLOR = {
-  0: 'rgba(0,0,0,1)',
-  1: 'rgba(0,0,170,1)',
-  2: 'rgba(0,170,0,1)',
-  3: 'rgba(0,170,170,1)',
-  4: 'rgba(170,0,0,1)',
-  5: 'rgba(170,0,170,1)',
-  6: 'rgba(255,170,0,1)',
-  7: 'rgba(170,170,170,1)',
-  8: 'rgba(85,85,85,1)',
-  9: 'rgba(85,85,255,1)',
-  a: 'rgba(85,255,85,1)',
-  b: 'rgba(85,255,255,1)',
-  c: 'rgba(255,85,85,1)',
-  d: 'rgba(255,85,255,1)',
-  e: 'rgba(255,255,85,1)',
-  f: 'rgba(255,255,255,1)',
-};
-
-function getHeight(message) {
+function getHeight(message: string) {
   const canvas = createCanvas(1, 1);
   const ctx = canvas.getContext('2d');
-  const splitMessageSpace: string[] = message.split(' ');
-  Object.entries(splitMessageSpace).forEach(([i, msg]) => {
+  const splitMessageSpace = message.split(' ');
+  splitMessageSpace.forEach((msg, i) => {
     if (!msg.startsWith('§')) splitMessageSpace[i] = `§r${msg}`;
   });
   const splitMessage = splitMessageSpace.join(' ').split(/§|\n/g);
@@ -48,12 +30,12 @@ function getHeight(message) {
   return height + 10;
 }
 
-function generateMessageImage(message) {
+export default function generateMessageImage(message: string) {
   const canvasHeight = getHeight(message);
   const canvas = createCanvas(1000, canvasHeight);
   const ctx = canvas.getContext('2d');
-  const splitMessageSpace: string[] = message.split(' ');
-  Object.entries(splitMessageSpace).forEach(([i, msg]) => {
+  const splitMessageSpace = message.split(' ');
+  splitMessageSpace.forEach((msg, i) => {
     if (!msg.startsWith('§')) splitMessageSpace[i] = `§r${msg}`;
   });
   const splitMessage = splitMessageSpace.join(' ').split(/§|\n/g);
@@ -66,7 +48,7 @@ function generateMessageImage(message) {
   let width = 5;
   let height = 35;
   Object.values(splitMessage).forEach((msg) => {
-    const colorCode = RGBA_COLOR[msg.charAt(0)];
+    const colorCode = rgbaColor[msg.charAt(0)];
     const currentMessage = msg.substring(1);
     if (width + ctx.measureText(currentMessage).width > 1000 || msg.charAt(0) === 'n') {
       width = 5;
@@ -80,5 +62,3 @@ function generateMessageImage(message) {
   });
   return canvas.toBuffer();
 }
-
-export default generateMessageImage;
