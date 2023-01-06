9c68575c55cb146a27f605386e17f6c7c9d8b31f
BridgeSense <bridgesensemc@gmail.com>
2023-01-06 15:36:52 +0800

    feat: allow more tracked guilds

--- src/helper/gexpWatch.ts
+++ src/helper/gexpWatch.ts
@@ -4,34 +4,37 @@ import Database from 'better-sqlite3';
 import config from '../config.json' assert { type: 'json' };
 import { channels } from '../events/discord/ready.js';
 import { doubleDigits } from './utils.js';
+import { NumberObject, StringObject } from '../types/global.d.js';
 
 const db = new Database('guild.db');
 
 function gexpGained(gained: number): [string, number] {
-  let desc;
-  let color;
   if (gained >= 0) {
-    desc = '<:up1:969182381485482064> Gained';
-    color = 0x2ecc70;
-  } else {
-    desc = '<:down:969182381162500097> Lost';
-    color = config.colors.red;
+    return ['<:up1:969182381485482064> Gained', config.colors.green];
   }
-  return [desc, color];
+  return ['<:down:969182381162500097> Lost', config.colors.red];
 }
 
 export default async function gexpWatch() {
   schedule('00 50 11 * * 0-6', async () => {
-    const urls = [
-      `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Dominance`,
-      `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Cronos`,
-      `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Rebel`,
-      `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=The%20Dawns%20Awakening`
-    ];
-    const guildGexp = [];
-    for (let i = 0; i < urls.length; i++) {
-      // eslint-disable-next-line no-await-in-loop
-      guildGexp.push((await (await fetch(urls[i])).json()).guild.exp);
+    const guildThumbnails: StringObject = {
+      rebel: `https://cdn.discordapp.com/attachments/986281342457237624/1001705614264778803/a_96a019775f60ebe70d0e5ea3d762ff57.webp`,
+      cronos: `https://cdn.discordapp.com/attachments/986281342457237624/1001839326033879080/ezgif-1-9402e80289.png`,
+      dawns: `https://cdn.discordapp.com/icons/406305465341837313/bdabc7a8bd14a701e104ec9800c12fd1.webp?size=4096&width=656&height=656`,
+      abyss: `https://cdn.discordapp.com/icons/549722930251300865/a_581a4ba9007bfc5b62d27e4bd89a0d67.webp?size=4096&width=656&height=656`,
+      lucid: `https://cdn.discordapp.com/avatars/1014274693596983326/9d5bddb15cd0b0ef69035198f1d68914.webp?size=4096&width=656&height=656`
+    };
+    const gexpUrls: StringObject = {
+      dominance: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Dominance`,
+      rebel: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Rebel`,
+      cronos: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Cronos`,
+      dawns: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=The%20Dawns%20Awakening`,
+      abyss: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=The%20Abyss`,
+      lucid: `https://api.hypixel.net/guild?key=${config.keys.hypixelApiKey}&name=Lucid`
+    };
+    const guildGexp: NumberObject = {};
+    for (const i in gexpUrls) {
+      guildGexp[i] = (await (await fetch(gexpUrls[i])).json()).guild.exp;
     }
     const date = new Date();
     const today = `${doubleDigits(date.getDate())}/${doubleDigits(date.getMonth() + 1)}/${date.getFullYear()}`;
@@ -39,122 +42,60 @@ export default async function gexpWatch() {
     const previous = `${doubleDigits(date.getDate())}/${doubleDigits(date.getMonth() + 1)}/${date.getFullYear()}`;
     date.setDate(date.getDate() - 6);
     const prevWeek = `${doubleDigits(date.getDate())}/${doubleDigits(date.getMonth() + 1)}/${date.getFullYear()}`;
-    date.setDate(date.getDate() - 30);
+    date.setDate(date.getDate() - 23);
     const prevMonth = `${doubleDigits(date.getDate())}/${doubleDigits(date.getMonth() + 1)}/${date.getFullYear()}`;
     const unix = Math.floor(Date.now() / 1000);
+    const embeds = [];
 
     // Dominance
-    let result = db.prepare('SELECT gexp FROM dominanceWatch WHERE date=?').get(previous).gexp;
-    let gained = guildGexp[0] - result;
-    db.prepare('INSERT INTO dominanceWatch (date, gexp, gained) VALUES (?, ?, ?)').run(today, guildGexp[0], gained);
-
-    // Rebel
-    result = db.prepare('SELECT separation FROM rebelWatch WHERE date=?').get(previous).separation;
-    let weekly = db.prepare('SELECT separation FROM rebelWatch WHERE date=?').get(prevWeek).separation;
-    let monthly = db.prepare('SELECT separation FROM rebelWatch WHERE date=?').get(prevMonth).separation;
-    let difference = guildGexp[0] - guildGexp[2];
-    gained = difference - result;
-    db.prepare('INSERT INTO rebelWatch (date, gexp, separation, gained) VALUES (?, ?, ?, ?)').run(
+    let gained = guildGexp.dominance - db.prepare('SELECT gexp FROM dominanceWatch WHERE date=?').get(previous).gexp;
+    db.prepare('INSERT INTO dominanceWatch (date, gexp, gained) VALUES (?, ?, ?)').run(
       today,
-      guildGexp[2],
-      difference,
+      guildGexp.dominance,
       gained
     );
-    const rebelEmbed = new EmbedBuilder()
-      .setColor(gexpGained(gained)[1])
-      .setTitle(`We are ${difference.toLocaleString()} GEXP ahead of Rebel`)
-      .setDescription(
-        `${gexpGained(gained)[0]} **${Math.abs(
-          gained
-        ).toLocaleString()}** GEXP\n<:clock_:969185417712775168> Recorded <t:${unix}:R>`
-      )
-      .addFields(
-        {
-          name: '<:week:982237517233414194> Weekly',
-          value: `${gexpGained(difference - weekly)[0]} **${Math.abs(difference - weekly).toLocaleString()}** GEXP`,
-          inline: true
-        },
-        {
-          name: '<:calendar_3d:1029713106550657055> Monthly',
-          value: `${gexpGained(difference - monthly)[0]} **${Math.abs(difference - monthly).toLocaleString()}** GEXP`,
-          inline: true
-        }
-      )
-      .setThumbnail(
-        'https://cdn.discordapp.com/attachments/986281342457237624/1001705614264778803/a_96a019775f60ebe70d0e5ea3d762ff57.webp'
-      );
 
-    // Cronos
-    result = db.prepare('SELECT separation FROM cronosWatch WHERE date=?').get(previous).separation;
-    weekly = db.prepare('SELECT separation FROM cronosWatch WHERE date=?').get(prevWeek).separation;
-    monthly = db.prepare('SELECT separation FROM cronosWatch WHERE date=?').get(prevMonth).separation;
-    difference = guildGexp[0] - guildGexp[1];
-    gained = difference - result;
-    db.prepare('INSERT INTO cronosWatch (date, gexp, separation, gained) VALUES (?, ?, ?, ?)').run(
-      today,
-      guildGexp[1],
-      difference,
-      gained
-    );
-    const cronosEmbed = new EmbedBuilder()
-      .setColor(gexpGained(gained)[1])
-      .setTitle(`We are ${difference.toLocaleString()} GEXP ahead of Cronos`)
-      .setDescription(
-        `${gexpGained(gained)[0]} **${Math.abs(
-          gained
-        ).toLocaleString()}** GEXP\n<:clock_:969185417712775168> Recorded <t:${unix}:R>`
-      )
-      .addFields(
-        {
-          name: '<:week:982237517233414194> Weekly',
-          value: `${gexpGained(difference - weekly)[0]} **${Math.abs(difference - weekly).toLocaleString()}** GEXP`,
-          inline: true
-        },
-        {
-          name: '<:calendar_3d:1029713106550657055> Monthly',
-          value: `${gexpGained(difference - monthly)[0]} **${Math.abs(difference - monthly).toLocaleString()}** GEXP`,
-          inline: true
-        }
-      )
-      .setThumbnail(
-        'https://cdn.discordapp.com/attachments/986281342457237624/1001839326033879080/ezgif-1-9402e80289.png'
+    for (const i in guildGexp) {
+      if (i === 'dominance') {
+        continue;
+      }
+      const daily = db.prepare(`SELECT separation FROM ${i}Watch WHERE date=?`).get(previous).separation;
+      const weekly = db.prepare(`SELECT separation FROM ${i}Watch WHERE date=?`).get(prevWeek).separation;
+      const monthly = db.prepare(`SELECT separation FROM ${i}Watch WHERE date=?`).get(prevMonth).separation;
+      const difference = guildGexp.dominance - guildGexp[i];
+      gained = difference - daily;
+      db.prepare(`INSERT INTO ${i}Watch (date, gexp, separation, gained) VALUES (?, ?, ?, ?)`).run(
+        today,
+        guildGexp[i],
+        difference,
+        gained
       );
-
-    // Dawns
-    result = db.prepare('SELECT separation FROM dawnsWatch WHERE date=?').get(previous).separation;
-    weekly = db.prepare('SELECT separation FROM dawnsWatch WHERE date=?').get(prevWeek).separation;
-    monthly = db.prepare('SELECT separation FROM dawnsWatch WHERE date=?').get(prevMonth).separation;
-    difference = guildGexp[0] - guildGexp[3];
-    gained = difference - result;
-    db.prepare('INSERT INTO dawnsWatch (date, gexp, separation, gained) VALUES (?, ?, ?, ?)').run(
-      today,
-      guildGexp[1],
-      difference,
-      gained
-    );
-    const dawnsEmbed = new EmbedBuilder()
-      .setColor(gexpGained(gained)[1])
-      .setTitle(`We are ${difference.toLocaleString()} GEXP ahead of The Dawns Awakening`)
-      .setDescription(
-        `${gexpGained(gained)[0]} **${Math.abs(
-          gained
-        ).toLocaleString()}** GEXP\n<:clock_:969185417712775168> Recorded <t:${unix}:R>`
-      )
-      .addFields(
-        {
-          name: '<:week:982237517233414194> Weekly',
-          value: `${gexpGained(difference - weekly)[0]} **${Math.abs(difference - weekly).toLocaleString()}** GEXP`,
-          inline: true
-        },
-        {
-          name: '<:calendar_3d:1029713106550657055> Monthly',
-          value: `${gexpGained(difference - monthly)[0]} **${Math.abs(difference - monthly).toLocaleString()}** GEXP`,
-          inline: true
-        }
-      )
-      .setThumbnail(
-        'https://cdn.discordapp.com/attachments/986281342457237624/1001839294165561505/9faa8886a380dbea8e35c053df43799e.webp'
+      embeds.push(
+        new EmbedBuilder()
+          .setColor(gexpGained(gained)[1])
+          .setTitle(`We are ${difference.toLocaleString()} GEXP ahead of ${i.charAt(0).toUpperCase() + i.slice(1)}`)
+          .setDescription(
+            `${gexpGained(gained)[0]} **${Math.abs(
+              gained
+            ).toLocaleString()}** GEXP\n<:clock_:969185417712775168> Recorded <t:${unix}:R>`
+          )
+          .addFields(
+            {
+              name: '<:week:982237517233414194> Weekly',
+              value: `${gexpGained(difference - weekly)[0]} **${Math.abs(difference - weekly).toLocaleString()}** GEXP`,
+              inline: true
+            },
+            {
+              name: '<:calendar_3d:1029713106550657055> Monthly',
+              value: `${gexpGained(difference - monthly)[0]} **${Math.abs(
+                difference - monthly
+              ).toLocaleString()}** GEXP`,
+              inline: true
+            }
+          )
+          .setThumbnail(guildThumbnails[i])
       );
-    await channels.guildWatch.send({ embeds: [rebelEmbed, cronosEmbed, dawnsEmbed] });
+    }
+    await channels.guildWatch.send({ embeds });
   });
 }
