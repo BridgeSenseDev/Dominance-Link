f576b641f9e87dd1c75d863a010585427c72f1da
BridgeSense <bridgesensemc@gmail.com>
2023-01-27 17:23:06 +0800

    feat: allow break members to rejoin through messages

--- src/events/minecraft/message.ts
+++ src/events/minecraft/message.ts
@@ -95,7 +95,6 @@ export default async function execute(client: Client, msg: string, rawMsg: strin
     db.prepare('UPDATE guildMembers SET messages = messages + 1 WHERE uuid = (?)').run(uuid);
     addXp(discordId);
   } else if (msg.includes('From')) {
-    let waitlist;
     let [, , name] = msg.split(' ');
     name = name.slice(0, -1);
     let uuid = await nameToUuid(name);
@@ -104,22 +103,10 @@ export default async function execute(client: Client, msg: string, rawMsg: strin
       name = name.slice(0, -1);
       uuid = await nameToUuid(name);
     }
-    try {
-      waitlist = db.prepare('SELECT discord, channel FROM waitlist WHERE uuid = ?').get(uuid);
-    } catch (err) {
-      /* empty */
-    }
-    if (waitlist) {
+    const waitlist = db.prepare('SELECT discord, channel FROM waitlist WHERE uuid = ?').get(uuid);
+    const breaks = db.prepare('SELECT discord, thread FROM breaks WHERE uuid = ?').get(uuid);
+    if (waitlist || breaks) {
       await chat(`/g invite ${name}`);
-      const channel = client.channels.cache.get(waitlist.channel) as TextChannel;
-      const embed = new EmbedBuilder()
-        .setColor(config.colors.discordGray)
-        .setTitle(`${name} has been invited to the guild`)
-        .setDescription(
-          'If you did not receive an invite:\n`-` Make sure you are not in a guild\n`-` The guild may be currently ' +
-            'full, check using the </online:1023548883332255765> command\nIf the guild is full, ping <@&1016513036313448579> here'
-        );
-      await channel.send({ embeds: [embed] });
     }
   } else if (msg.includes('The Guild has reached Level')) {
     const level = msg.split(' ')[msg.split(' ').indexOf('Level') + 1];
@@ -219,7 +206,7 @@ export default async function execute(client: Client, msg: string, rawMsg: strin
     });
   } else if (msg.includes('joined the guild!')) {
     let funFact;
-    const name = msg.substring(msg.search(/ (.*?) joined/g) + 1, msg.lastIndexOf(' joined'));
+    const name = msg.split(' ')[msg.split(' ').indexOf('joined') - 1];
     const funFacts = await (
       await fetch('https://api.api-ninjas.com/v1/facts?limit=3', {
         method: 'GET',
@@ -297,11 +284,47 @@ export default async function execute(client: Client, msg: string, rawMsg: strin
         `<a:wave_animated:1036265311390928897> Welcome to Dominance, ${name}! Our current gexp requirement is ${config.guild.gexpReq} per week. ${funFacts[2].fact}`
       );
     }
+  } else if (msg.includes('have 5 minutes to accept') && msg.includes('invite')) {
+    let name;
+    if (msg.includes('offline invite')) {
+      name = msg.split(' ')[msg.split(' ').indexOf('They') - 1].slice(0, -1);
+    } else {
+      name = msg.split(' ')[msg.split(' ').indexOf('to') - 1];
+    }
+    const uuid = await nameToUuid(name);
+    const waitlist = db.prepare('SELECT discord, channel FROM waitlist WHERE uuid = ?').get(uuid);
+    const breaks = db.prepare('SELECT discord, thread FROM breaks WHERE uuid = ?').get(uuid);
+    const embed = new EmbedBuilder()
+      .setColor(config.colors.discordGray)
+      .setDescription(`${name} has been invited to the guild`);
+    if (waitlist) {
+      const channel = client.channels.cache.get(waitlist.channel) as TextChannel;
+      await channel.send({ content: `<@${waitlist.discord}>`, embeds: [embed] });
+    } else if (breaks) {
+      const channel = client.channels.cache.get(breaks.thread) as ThreadChannel;
+      await channel.send({ content: `<@${breaks.discord}>`, embeds: [embed] });
+    }
+  } else if (msg.includes('is already in another guild!')) {
+    const name = msg.split(' ')[msg.split(' ').indexOf('is') - 1];
+    const uuid = await nameToUuid(name);
+    const waitlist = db.prepare('SELECT discord, channel FROM waitlist WHERE uuid = ?').get(uuid);
+    const breaks = db.prepare('SELECT discord, thread FROM breaks WHERE uuid = ?').get(uuid);
+    const embed = new EmbedBuilder()
+      .setColor(config.colors.discordGray)
+      .setTitle(`${name} is in another guild`)
+      .setDescription(`Please leave your current guild`);
+    if (waitlist) {
+      const channel = client.channels.cache.get(waitlist.channel) as TextChannel;
+      await channel.send({ content: `<@${waitlist.discord}>`, embeds: [embed] });
+    } else if (breaks) {
+      const channel = client.channels.cache.get(breaks.thread) as ThreadChannel;
+      await channel.send({ content: `<@${breaks.discord}>`, embeds: [embed] });
+    }
   }
   if (msg.includes('left the guild!') || msg.includes('was kicked')) {
-    let name = msg.substring(msg.search(/ (.*?) left/g) + 1, msg.lastIndexOf(' left'));
+    let name = msg.split(' ')[msg.split(' ').indexOf('left') - 1];
     if (!name) {
-      name = msg.substring(msg.search(/ (.*?) was/g) + 1, msg.lastIndexOf(' was'));
+      name = msg.split(' ')[msg.split(' ').indexOf('was') - 1];
     }
     const uuid = await nameToUuid(name);
     const { discord } = db.prepare('SELECT discord FROM members WHERE uuid = ?').get(uuid);
