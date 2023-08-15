4946979fd19efc778ceaf766539db7a2cdbdff71
bagelwaffle <dev@bagelwaffle.com>
2023-08-14 20:47:34 -0700

    remove music commands

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -1,9 +1,1 @@
 import { Collection } from 'discord.js';
-import { DisTube } from 'distube';
-
-declare module 'discord.js' {
-  export interface Client {
-    commands: Collection<unknown, any>;
-    distube: DisTube;
-  }
-}
