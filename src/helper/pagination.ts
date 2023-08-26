fcd1ba26a6d6b04622bcc44ec7b8a857748a0bdd
BridgeSense <bridgesensemc@gmail.com>
2023-08-26 21:15:03 +0800

    feat: /count and pagination

---
+++ src/helper/pagination.ts
@@ -0,0 +1,63 @@
+import {
+  ButtonStyle,
+  CacheType,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  MessageActionRowComponentBuilder,
+  ComponentType
+} from 'discord.js';
+
+export default async function pagination(
+  interaction: ChatInputCommandInteraction<CacheType>,
+  embeds: EmbedBuilder[],
+  actionsRows?: ActionRowBuilder<MessageActionRowComponentBuilder>[]
+) {
+  const paginatorRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setCustomId('leftPage')
+      .setEmoji('<:left_arrow:1144817673293811742>')
+      .setStyle(ButtonStyle.Danger)
+      .setDisabled(true),
+    new ButtonBuilder()
+      .setCustomId('rightPage')
+      .setEmoji(':right_arrow:1144817676569542656')
+      .setStyle(ButtonStyle.Success)
+  );
+
+  let components = actionsRows ? [...actionsRows.flat(), paginatorRow] : [paginatorRow];
+  const message = await interaction.editReply({ embeds: [embeds[0]], components });
+  let page = 0;
+
+  const collector = message.createMessageComponentCollector({
+    componentType: ComponentType.Button,
+    time: 5 *  60 * 1000
+  });
+
+  collector.on('collect', async (collectorInteraction) => {
+    if (collectorInteraction.customId === 'leftPage') {
+      page--;
+    } else if (collectorInteraction.customId === 'rightPage') {
+      page++;
+    }
+
+    if (page === embeds.length - 1) {
+      paginatorRow.components[1].setDisabled(true);
+    } else {
+      paginatorRow.components[1].setDisabled(false);
+    }
+    if (page === 0) {
+      paginatorRow.components[0].setDisabled(true);
+    } else {
+      paginatorRow.components[0].setDisabled(false);
+    }
+
+    components = actionsRows ? [...actionsRows.flat(), paginatorRow] : [paginatorRow];
+    await collectorInteraction.update({ embeds: [embeds[page]], components });
+  });
+
+  collector.on('end', async () => {
+    await interaction.editReply({ embeds: [embeds[page]], components: [] });
+  });
+}
---
+++ src/slashCommands/count.ts
@@ -0,0 +1,60 @@
+import { SlashCommandBuilder, EmbedBuilder, ChatInputCommandInteraction } from 'discord.js';
+import Database from 'better-sqlite3';
+import config from '../config.json' assert { type: 'json' };
+import { Count } from '../types/global';
+import { discordToUuid, uuidToName } from '../helper/utils';
+import pagination from '../helper/pagination';
+
+const db = new Database('guild.db');
+
+export const data = new SlashCommandBuilder().setName('count').setDescription('View counting stats');
+export async function execute(interaction: ChatInputCommandInteraction) {
+  await interaction.deferReply({fetchReply: true});
+
+  const counts = db.prepare('SELECT * FROM counting').all() as Count[];
+
+  const userCountsArray = Object.entries(
+    counts.reduce(
+      (acc: Record<string, number>, { discord }: Count) => ({ ...acc, [discord]: (acc[discord] || 0) + 1 }),
+      {}
+    )
+  )
+    .map(([discord, count]: [string, number]) => ({ discord, count }))
+    .sort((a: Count, b: Count) => b.count - a.count);
+
+  const pages = Array.from({ length: Math.ceil(userCountsArray.length / 12) }, (_, i) =>
+    userCountsArray.slice(i * 12, i * 12 + 12)
+  );
+
+  const embeds: EmbedBuilder[] = [];
+
+  for (const [i, page] of pages.entries()) {
+    const embed = new EmbedBuilder()
+      .setColor(config.colors.discordGray)
+      .setTitle('Counting Leaderboards')
+      .setFooter({ text: `Page ${i + 1} of ${pages.length}` });
+
+    for (const [j, { discord, count }] of page.entries()) {
+      const place = i * 10 + j + 1;
+      let customEmoji;
+      if (place === 1) {
+        customEmoji = config.emojis.gold;
+      } else if (place === 2) {
+        customEmoji = config.emojis.silver;
+      } else if (place === 3) {
+        customEmoji = config.emojis.bronze;
+      } else {
+        customEmoji = config.emojis.bullet;
+      }
+      embed.addFields({
+        name: `#${place} - ${await uuidToName(discordToUuid(discord)!)}`,
+        value: `${customEmoji} ${count}`,
+        inline: true
+      });
+    }
+
+    embeds.push(embed);
+  }
+
+  await pagination(interaction, embeds)
+}
